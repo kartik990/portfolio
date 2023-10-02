5f29e3ffd798d4a2d9443859a1ad704e02f4c8ef
kartik rai <rai.kartik99@gmail.com>
2023-10-02 19:05:01 +0530

    updated resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
           </Link>
 
           <a
-            href="https://drive.google.com/file/d/1PBFNmRHJM0idXWFtu2NtaW4_poUCDGBn/view?usp=sharing"
+            href="https://drive.google.com/file/d/11J1LWduLHvi4cwAARzo2EEYWL64q7_3Z/view?usp=sharing"
             rel="noreferrer"
             target="_blank"
           >
